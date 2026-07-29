971123e65e50c3f336e9b690fcf1ba4a86e97856
Sambal404 <luthrobbyantoilham@gmail.com>
2026-07-29 11:29:43 +0700

    chore: add trigger untuk setelah checkout akan otomatis merubah status kamar

---
+++ backend/database/triggers/trg_after_checkout.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+
+CREATE TRIGGER trg_auto_room_dirty_after_checkout
+AFTER UPDATE ON reservation_rooms
+FOR EACH ROW
+BEGIN
+    -- Jika status sebelumnya bukan checked_out, dan status yang baru adalah checked_out
+    IF OLD.status != 'checked_out' AND NEW.status = 'checked_out' THEN
+        UPDATE rooms
+        SET status = 'available', 
+            clean_status = 'dirty', 
+            updated_at = CURRENT_TIMESTAMP
+        WHERE id = NEW.room_id;  -- NEW.room_id adalah room_id dari row reservation_rooms yang terupdate
+    END IF;
+END //
+
+DELIMITER ;
